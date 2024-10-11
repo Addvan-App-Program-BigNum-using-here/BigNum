190c31c35cc22bf86ec19ffaaea3b8b4cb9c33e5
Boomwin <20192223@kookmin.ac.kr>
2024-10-11 18:44:33 +0900

    edit x

--- ref/util.h
+++ ref/util.h
@@ -2,15 +2,16 @@
 #include "error_msg.h"
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
-msg bi_new(bigint** dst, int word_len);
-msg bi_delete(bigint** dst);
-msg bi_refine(bigint* src);
-msg bi_assign(bigint** dst, bigint* src);
+msg bi_new(bigint **dst, int word_len);
+msg bi_delete(bigint **dst);
+msg bi_refine(bigint *src);
+msg bi_assign(bigint **dst, bigint *src);
 
-msg bi_set_from_array(bigint** dst, int sign, int word_len, word* data, int endian);
-msg bi_set_from_string(bigint** dst, char* int_str, int base);
+msg bi_set_from_array(bigint **dst, int sign, int word_len, word *data, int endian);
+msg bi_set_from_string(bigint **dst, char *int_str, int base);
 
-msg bi_get_random(bigint** dst, int word_len);
+msg bi_get_random(bigint **dst, int word_len);
 
-msg bi_print(bigint* dst, int base);
+msg bi_print(bigint *dst, int base);
