06a19f766d0158c85684715fde4e2576640ee26d
jinhyeon721 <qlcwlsgus721@kookmin.ac.kr>
2024-10-15 20:49:45 +0900

    Add random fucntion

--- ref/random.c
+++ ref/random.c
@@ -8,15 +8,49 @@
 * Arguments:   - bigint** dst: pointer to bigint struct
 *              - int word_len: length of bigint struct
 **************************************************/
-msg bi_get_random(bigint** dst, int word_len){
-    msg result_msg = bi_new(dst, word_len);
-    if(result_msg == MEM_ALLOC_FAIL){
-        return RANDOM_FAIL;
-    }
+typedef uint8_t byte; // 8-bit �ڷ��� ����
+msg bi_get_random(bigint** dst, int word_len) {
+	bi_new(*dst, word_len); // word_len ���̸� ���� bigint �޸� �Ҵ�
 
-    (*dst)->sign = rand() % 2;
-    for(int i = 0; i < word_len; i++){
-        (*dst)->a[i] = rand() & 0xFFFFFFFF;
-    }
-    return RANDOM_SUCCESS;
+	//cnt : �ʿ��� ������ ����
+	int cnt = word_len * (sizeof(word) / sizeof(byte)); // �迭�� ������ 1byte�� ���� => �� word_len * 4�� cnt �ʿ�
+
+	(*dst)->sign = rand() & 1;
+
+	while (cnt > 0) {
+		for (int i = 0; i < 4; i++) { // word �迭�� ũ�� : 32-bit, rand() & 0xff�� ũ�� : 8-bit => �� 4�� �ݺ�
+			(*dst)->a[cnt] += (rand() & 0xff) << (8* i); 
+		}
+
+		cnt--;
+	}
+
+	bi_refine(*dst); // ���ʿ��� 0 �����ؼ� �޸� ����ȭ
+}
+
+
+msg array_rand(word* dst, int wordlen){
+	
+}
+
+msg bi_get_random_ver2(bigint** dst, int word_len){
+	(byte*)
+}
+int main()
+{	
+	srand(time(NULL));
+
+	//puts("HELLO KMU!!");
+
+	bigint a, b, c;
+
+	bi_new(&a, 16);
+	bi_new(&b, 16);
+	bi_get_random(&a, 16);
+	bi_get_random(&b, 16);
+
+	/*bi_set_from_string(&a, "0x12321328378213", 16);
+	bi_set_from_string(&b, "0x1ffffffff78213", 16);*/
+	//bi_assign(&a, &b);
+	return 0;
 }
