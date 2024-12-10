dea3882100df5605b8300ba82fa99f7fb63c774b
jinhyeon721 <qlcwlsgus721@kookmin.ac.kr>
2024-12-10 09:07:00 +0900

    12 10

--- ref/test/test_main.h
+++ ref/test/test_main.h
@@ -1,10 +1,19 @@
 #ifndef TEST_MAIN_H
 #define TEST_MAIN_H
+#include <stdio.h>
+#ifdef _WIN32
+    #define POPEN _popen
+    #define PCLOSE _pclose
+    #include <windows.h>
+#else
+    #define POPEN popen
+    #define PCLOSE pclose
+#endif
 
 #define BITS_SIZE 1024                              // 테스트 비트 크기
 #define test_word_size BITS_SIZE / WORD_BITS               // 0인 경우 테스트 시 사용되는 word 크기는 랜덤
 #define test_word_size_limit BITS_SIZE / WORD_BITS          // 랜덤 word 크기 사용 시 제한 범위
-#define test_size  10                                   // 테스트 횟수
+#define test_size  1                             // 테스트 횟수
 #define barret_word_size test_word_size                // barret N 크기
 
 #include <stdio.h>
@@ -13,6 +22,8 @@
 #include "operate_test.h"
 #include "crypto_test.h"
 #include "random_test.h"
+
+
 
 /**
  * @brief Clear test file
